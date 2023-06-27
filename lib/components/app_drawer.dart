f50e01df2b636d60a688139e8a5600e79285412f
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2023-06-27 14:14:49 -0300

    finally project store

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/auth.dart';
+import 'package:shop/pages/orders_page.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/utils/custom_route.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
@@ -31,6 +35,9 @@ class AppDrawer extends StatelessWidget {
               Navigator.of(context).pushReplacementNamed(
                 AppRoutes.orders,
               );
+              // Navigator.of(context).pushReplacement(
+              //   CustomRoute(builder: (ctx) => const OrdersPage()),
+              // );
             },
           ),
           const Divider(),
@@ -40,6 +47,20 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context).pushReplacementNamed(
                 AppRoutes.products,
+              );
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Sair'),
+            onTap: () {
+              Provider.of<Auth>(
+                context,
+                listen: false,
+              ).logout();
+              Navigator.of(context).pushReplacementNamed(
+                AppRoutes.authOrHome,
               );
             },
           ),
